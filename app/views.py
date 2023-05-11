ecd48a8735309d058616e6abe84659ebefbae69b
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-05-11 18:49:44 +0300

    minor changes to html

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from flask import render_template, jsonify, request, make_response,  send_file
 
 
 @app.route("/")
-def home():
-    return render_template("public/index.html")
+def view_invoice():
+    return render_template("public/invoice.html")

72772866b0ed6100316e992d9b34f846c00e7a9f
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-05-16 23:13:20 +0300

    invoice frontend updates

--- app/static/js/invoice.js
+++ app/static/js/invoice.js
@@ -25,4 +25,37 @@ document.querySelector(".date-input>input").addEventListener('change', () => {
     console.log('some change');
     console.log(document.querySelector(".date-input>input").value);
     document.querySelector(".date-input>p").innerHTML = document.querySelector(".date-input>input").value
-});  
+});
+
+document.querySelector('#add-line').addEventListener('click', ()=>{
+    console.log("click");
+    control = document.querySelector('#add-line-holder')
+    block = document.querySelector('.inv-lines')
+    line = document.createElement("div");
+    line.classList.add('invoice-line');
+    line.innerHTML = `
+    <button class="remove-line">-</button>
+    <input class="product" maxlength="50" type="text" placeholder="Produktas">
+    <input class="vnt" type="number" min="0" value="1" placeholder="1">
+    <input type="text" maxlength="5" value="vnt">
+    <input class="price" type="number" min="0" value="1" placeholder="1">
+    <input class="total" type="number" readonly value="1">
+    `
+    block.insertBefore(line, control);
+    remove_button_usability()
+
+})
+
+function remove_button_usability(){
+    remove_buttons = document.querySelectorAll('.remove-line')
+    remove_button = remove_buttons[remove_buttons.length-1]
+    .addEventListener('click',(e)=>{
+        console.log("click -");
+        if (window.confirm("Ar norite ištrinti visa eilutė?")){
+            e.target.parentElement.remove()
+        }
+    })
+}
+
+remove_button_usability()
+
